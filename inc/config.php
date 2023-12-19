b23b0c6c42336647c65a9cbb81ee4bdc47041940
systemd <47719596+systemd1337@users.noreply.github.com>
2023-12-19 18:14:51 -0500

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1481,16 +1481,16 @@
 
 	// "## Mod" makes everything purple, including the name and tripcode:
 	$config['custom_capcode']['Mod'] = array(
-		'<span class="capcode" style="color:purple"> ## %s</span>',
+		'<span style="display: inline-block; font-size: 80 font-weight: normal; background: #4B0082	; color: #ffffff; padding: 2px 5px 2px 5px; margin-left: 2px; margin-right: 2px; margin-bottom: 2px; border-radius: 4px;}color:purple">## Mod</span>',
 		'color:purple', // Change name style; optional
 		'color:purple' // Change tripcode style; optional
 	);
 
 	// "## Admin" makes everything red and bold, including the name and tripcode:
 	$config['custom_capcode']['Admin'] = array(
-		'<span class="capcode" style="color:red;font-weight:bold"> ## %s</span>',
-		'color:red;font-weight:bold', // Change name style; optional
-		'color:red;font-weight:bold' // Change tripcode style; optional
+		'<span style="display: inline-block; font-size: 80 font-weight: normal; background: #6CB4EE; color: #ffffff; padding: 2px 5px 2px 5px; margin-left: 2px; margin-right: 2px; margin-bottom: 2px; border-radius: 4px;}color:blue">## Administrator</span>',
+		'color:blue;font-weight:bold', // Change name style; optional
+		'color:blue;font-weight:bold' // Change tripcode style; optional
 	);
 
 	// Enable the moving of single replies
